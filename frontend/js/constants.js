24c604c02d9c722cf0b60765f9937b78d97a9c47
Tarrrb <103268205+Tarrrb@users.noreply.github.com>
2022-05-26 13:52:49 +0200

    2a

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,13 +1,12 @@
 const contractAddress = "0x288493A77903199d5089751C84726575F35c893e"; //Replace with your own contract address
 const chain = 'polygon'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
-const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
-const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
-const h1_presale_mint = "Pre-Sale Minting Open!!";
-const h1_public_mint = "Public Minting Open!!";
-const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
+const welcome_h1 = "Välkommen till Mintkulan!";
+const welcome_h2 = "Koppla din metamask";
+const h1_presale_coming_soon = "Snart öppnar Blockmarknaden.se!";
+const h1_presale_mint = "Nordsken NFT lotteri";
+const h1_public_mint = "Nordskens NFT Lotteri";
+const h2_presale_coming_soon = "ji";
 const h2_presale_mint = "Public Minting Countdown";
 const p_presale_coming_soon = "We are working hard to launch the NFT Collection. Stay tuned for updates!";
 const p_presale_mint_not_whitelisted = "You are not whitelisted for the pre-sale.. 😢";
